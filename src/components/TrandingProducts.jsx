1eab28bffb42ca3d8cc85c7acf7128b934807f34
SharminAkter3 <mssharmin4145@gmail.com>
2024-02-25 16:33:27 +0600

    showing all tranding flower

--- src/components/TrandingProducts.jsx
+++ src/components/TrandingProducts.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import { domain } from "../env";
+import React, { useEffect, useState } from 'react'
+import { Grid } from '@material-ui/core';
+import Headline from '../common/Headline';
+import SingleProduct from '../common/SingleProduct';
 
 const TrandingProducts = () => {
+    const [products, setProducts] = useState(null);
+    useEffect(() => {
+        const getTrandingProducts = async () => {
+            await axios({
+                url: `${domain}/api/trandingflower/`,
+                method: 'GET'
+            }).then(response => {
+                setProducts(response.data)
+                console.log('TrandingFlower===', response.data);
+            }).catch(e => {
+                console.log('TrandingFlower==', e);
+            })
+        }
+        getTrandingProducts()
+    }, [])
     return (
-        <div>
-            <h1>Tranding Flower</h1>
-        </div>
+        <Grid container spacing={2}   >
+            <Headline title="Trending" subtitle="Flowers" />
+            {
+                products?.map((item, i) => <Grid key={i} md={2} sm={4} item>
+                    <SingleProduct product={item?.flowers} />
+                </Grid>)
+            }
+        </Grid>
     )
 }
 
