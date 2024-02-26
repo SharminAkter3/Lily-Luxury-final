dd5b44bdfd0e61da5fc3673ec252e3eaaa17242b
SharminAkter3 <mssharmin4145@gmail.com>
2024-02-26 20:11:49 +0600

    navbar folishing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import { AppBar, Grid, IconButton, InputBase, Paper, Toolbar, Typography } from '@material-ui/core'
+import { AppBar, Badge, Button, Card, ClickAwayListener, Grid, IconButton, InputBase, MenuItem, Paper, Toolbar, Typography } from '@material-ui/core'
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useNavigate } from "react-router-dom";
 import SearchIcon from '@material-ui/icons/Search'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded'
+
 
 
 
 const Navbar = () => {
     const [text, setText] = useState('');
+    const [showMenu, setShowMenu] = useState(false);
     const navigate = useNavigate();
     const search = () => {
         navigate(`/query/${text}`);
@@ -41,6 +45,36 @@ const Navbar = () => {
 
                 </Grid>
 
+                {/* <Button color='inherit' onClick={() => { history.push('/login') }} >Login</Button>  */}
+                <Button color='inherit' style={{ backgroundColor: 'white', color: '#a32d58', padding: '5px', fontWeight: 'bold' }}>Login</Button>
+
+
+
+                <IconButton color="inherit" style={{ backgroundColor: '#a32d58', color: 'white', marginLeft: '10px', padding: '5px' }}>
+                    <Badge badgeContent="3" color="secondary">
+                        <ShoppingCartIcon />
+                    </Badge>
+                </IconButton>
+
+
+
+                <IconButton onClick={() => setShowMenu(true)} color="inherit" style={{ backgroundColor: '#a32d58', color: 'white', marginLeft: '10px', padding: '5px' }}>
+                    <AccountCircleRoundedIcon />
+                </IconButton>
+
+                {
+                    showMenu &&
+                    <ClickAwayListener onClickAway={() => setShowMenu(false)}>
+                        <Card style={{
+                            position: 'fixed',
+                            top: '60px',
+                            right: '10px'
+                        }}>
+                            <MenuItem style={{ color: '#a32d58', fontWeight: 'bold' }}>Profile</MenuItem>
+                            <MenuItem style={{ color: '#a32d58', fontWeight: 'bold' }}>Logout</MenuItem>
+                        </Card>
+                    </ClickAwayListener>
+                }
 
             </Toolbar >
 
